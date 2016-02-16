8c8b5aa2bd8f12c983de5186075cb01b85d05540
Esti Tweg <estictweg@hotmail.com>
2016-02-16 00:55:17 -0500

    Python source code to overlay one image on to another

---
+++ ImageOverlay.py
@@ -0,0 +1,57 @@
+#Collier, R. "Lectures Notes for COMP1405B – Introduction to Computer Science I" [PDF document].
+#Retrieved from cuLearn: https://www.carleton.ca/culearn/ (Fall 2015).
+
+#Stephenson, B. "Tutorial 2: Using Images with the SimpleGraphics Library" [PDF document].
+#Retrieved from cuLearn: https://www.carleton.ca/culearn/ (Fall 2015).
+
+
+from SimpleGraphics import *  #import simple graphics 
+    
+def main():
+    bg=input("What background picture would you like to use?")
+    gif=input("What picture would you like to overlay?")
+    topPic=loadImage(gif) #load image
+    halfW=int(getWidth(topPic)/2) #calculate half of the width of the topPic
+    halfH=int(getHeight(topPic)/2)#calculate half of the hieght of the topPic
+    x=0#set starting coordinates that are outside of range
+    y=0#set starting coordinates that are outside of range
+    #continue until user provides valid input that will allow the topPic to be fully displayed
+    while x<halfW or x>800-halfW or y<halfH or y>600-halfH: 
+        x=int(input("How far to the right do you want the image?"))
+        y=int(input("How far to the left do you want the image?"))
+        #if x and y are putside of range
+        if x<halfW or x>800-halfW or y<halfH or y>600-halfH: 
+            print("That is not a valid coordinate, try again") #error messges
+        images(bg,gif,x,y)
+    
+
+
+def images(bg,gif,x,y):
+    bg=loadImage(bg) #load the background image
+    topPic=loadImage(gif) 
+    bgW=int(getWidth(bg)) #calculate the width of the background
+    bgH=int(getHeight(bg)) #calculate the height of the background
+    bgX = int(getWidth() / 2 - bgW / 2) #x coordinate for it to be centered
+    bgY = int(getHeight() / 2 - bgH / 2)#y coordinate for it to be centered
+    drawImage(bg, bgX,bgY) #display centered background
+
+    topPicW = int(getWidth(topPic)) #get the width of topPic
+    topPicH= int(getHeight(topPic)) #get the height of topPic
+    NewImg=createImage(topPicW,topPicH) #make a new image that has the same dementions as topPic
+ 
+     #run through every pixel in the image
+    for x1 in range (0,topPicW): 
+        for y1 in range (0,topPicH):
+            r,g,b=getPixel(topPic,x1,y1) #get colours for each pixel
+            r1,g1,b1=getPixel(bg,x+x1-int(topPicW/2), y+y1-int(topPicH/2)) #colours for each pixel from bg
+        
+            if not (r<=130 and g>=140 and b<=130): # if it is not green
+                r=int((r+r1)/2) #get the average of the bg and topPic for red,
+                g=int((g+g1)/2) #green
+                b=int((b+b1)/2) #and blue
+                putPixel(NewImg,x1,y1,r,g,b) #change the pixel colours to look transparent
+     
+    drawImage(NewImg,x-topPicW/2,y-topPicH/2) #draw the new transparent image 
+
+main() 
+
